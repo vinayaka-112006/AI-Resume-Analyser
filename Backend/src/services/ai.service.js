666d193d2c3c5d5fff21df5025dee22e4f24664f
vinayaka-112006 <v19468833@gmail.com>
2026-04-28 23:52:53 +0530

    Refactor PDF generation to use puppeteer-core
    Updated puppeteer import to use puppeteer-core and added chromium configuration for launching the browser.

--- Backend/src/services/ai.service.js
+++ Backend/src/services/ai.service.js
@@ -1,7 +1,9 @@
 const { GoogleGenAI } = require("@google/genai");
-const puppeteer = require("puppeteer");
+// const puppeteer = require("puppeteer");
 const { z } = require("zod");
 const { zodToJsonSchema } = require("zod-to-json-schema");
+const puppeteer = require("puppeteer-core");
+const chromium = require("@sparticuz/chromium");
 
 const ai = new GoogleGenAI({
   apiKey: process.env.GOOGLE_GENAI_API_KEY,
@@ -242,7 +244,12 @@ ${jobDescription}
 }
 
 async function generatePdfFromHtml(htmlContent) {
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({
+  args: chromium.args,
+  defaultViewport: chromium.defaultViewport,
+  executablePath: await chromium.executablePath(),
+  headless: chromium.headless,
+});
   const page = await browser.newPage();
   await page.setContent(htmlContent, { waitUntil: "networkidle0" });
   const pdfBuffer = await page.pdf({
