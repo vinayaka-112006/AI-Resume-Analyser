dc8ac05e19abd6fb4510f288abb284d029a205e7
vinayaka-112006 <v19468833@gmail.com>
2026-04-28 22:58:36 +0530

    Update CORS origin for production deployment

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -8,7 +8,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://ai-resume-analyser-frontend-1iyb.onrender.com",
     credentials: true,
   }),
 );
