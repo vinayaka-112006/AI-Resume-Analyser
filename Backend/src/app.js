bbb208ff12fd7fb67c2b29ae8e1777fd0322267f
vinayaka-112006 <v19468833@gmail.com>
2026-07-25 14:42:04 +0530

    Update CORS origin for frontend application

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -10,7 +10,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "https://ai-resume-analyser-frontend-1iyb.onrender.com",
+    origin: "ai-resume-analyser-three-hazel.vercel.app",
     credentials: true,
   }),
 );
